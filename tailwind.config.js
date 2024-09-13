ca7ade03dabe769133674455fec4ece2f022d4a9
riley <mechamriley@gmail.com>
2024-09-12 19:21:41 -0600

    Revert "resolved merge confilct with frabri branch"
    This reverts commit 15109e62dc8b86f8119e2e2a1a0d8ee1c414a2ac, reversing
    changes made to d168ebab0f91d5c503008f43d7713b9d50bd2c9e.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'unchecked': "url('assets/unchecked.pgn')",
-        'checked': "url('assets/checked.pgn')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
