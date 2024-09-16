3a253dbb96be4de52188cfcdabc491dfcb7f57ad
riley <mechamriley@gmail.com>
2024-09-15 20:38:56 -0600

    completely rewrote script.js and added a bit to index

--- script.js
+++ script.js
@@ -1,97 +1,69 @@
 //variables for the list elements
+const lists = document.querySelectorAll("#task-list, #doing, #completed");
 const todo = document.getElementById("task-list");
 const doing = document.getElementById("doing");
 const done = document.getElementById("completed");
 
 //variables for the modal
-const select = document.querySelector("select");
 const modal = document.getElementById("task-modal");
-const addOption = document.getElementById("#trigger-modal");
+const addOption = document.getElementById("trigger-modal");
 const submit = document.getElementById("submitBtn");
 const closeModal = document.getElementById("close-modal");
-const modalTask = document.getElementById("modalInput");
+const modalTask = document.getElementById("modal-input");
+const modalTaskPriority = document.getElementById("priority-list");
+const modalTaskStatus = document.getElementById("status");
 
 //variables for the add and delete buttons
-const deleteTask = document.getElementsByClassName("delete");
+const deleteTask = document.querySelector(".delete");
 const addBtn = document.getElementById("addBtn")
 
 //variables for the created task elements
-const taskItem = document.getElementsByClassName("task-item");
+let taskItems = [];
 
-// array for storing the tasks
+// array for storing the task data
 let taskStorage = [];
-//object for storing tasks
-let taskObject = {
-    task: "",
-    priority: "",
-    status: ""
-};
 
-// creates a task for the dropdown option selected
-function checkForCustomOption(selectElement) {
-    // Check if the "Add your own" option is selected
-    if (selectElement.value === "add") {
-        modal.style.display = "block";
-        // the modal will add the option so this is not needed
-        // Prompt the user for a new option
-        // const newOptionText = selectElement.value;
-
-        // if (newOptionText) {
-        //     // Create a new option element
-        //     const newOption = document.createElement("option");
-        //     newOption.text = newOptionText;
-        //     newOption.value = newOptionText.toLowerCase();
-        //     // Insert the new option before the "Add your own" option
-        //     selectElement.add(newOption, selectElement.options[selectElement.options.length - 1]);
-        //     // Set the newly added option as the selected one
-        //     selectElement.value = newOption.value;
-        // } else {
-        //     // If the user cancels the prompt, revert the selection
-        //     selectElement.value = selectElement.options[0].value;
-        // }
-    }
+// function to store and retrieve taskStorage in local storage
+function storeTasksInLocalStorage() {
+    localStorage.setItem("tasks", JSON.stringify(taskStorage));
 }
 
-// Load tasks from local storage
-loadTasksFromLocalStorage();
+function getTasksFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("tasks"));
+}
 
-closeModal.addEventListener("click", function () {
-    modal.style.display = "none";
-});
+// function that takes the modal input and stores it to the taskStorage array
+function storeModalInput() {
+    taskObject = {
+        task: modalTask.value,
+        priority: modalTaskPriority.value,
+        status: modalTaskStatus.value
+    }
+    taskStorage.push(taskObject);
+    storeTasksInLocalStorage();
+}
 
-submit.addEventListener("click", function () {
-    modal.style.display = "none";
-    // when submit is clicked the new task is added to the list
-    createTask(modalTask.value, todo);
-});
+// function that takes the dropdown selection and stores it to the taskStorage array
+function storeDropdownSelection(event) {
+    taskObject = {
+        task: event.value,
+        priority: "",
+        status: ""
+    }
+    if (taskObject.status === "" || taskObject.priority === "") {
+        taskObject.status = "todo";
+        taskObject.priority = "low";
+    }
+    taskStorage.push(taskObject);
+    storeTasksInLocalStorage();
+}
 
-addBtn.addEventListener("click", function (event) {
-    event.preventDefault();
+//function that shows the modal
+function showModal() {
     modal.style.display = "block";
-});
+}
 
-// Call the checkForCustomOption function when the select element value is "add"
-document.querySelectorAll("#work-list, #personal-list, #family-list, #home-list, #health-list, #financial-list, #learning-list").forEach(function (selectElement) {
-    selectElement.addEventListener("change", function (event) {
-        event.preventDefault();
-        checkForCustomOption(event.target);
-        createTask(event.target.value, todo);
-    });
-});
-
-// unecessary code. the addDeleteFunctionality function is called in the createTask function
-// Array.from(deleteTask).forEach(function (deleteButton) {
-//     deleteButton.addEventListener("click", function () {
-//         const taskItem = deleteButton.parentNode;
-//         taskItem.parentNode.removeChild(taskItem);
-//         const taskIndex = taskStorage.indexOf(taskItem.textContent.replace("Delete", "").trim());
-//         if (taskIndex > -1) {
-//             taskStorage.splice(taskIndex, 1);
-//         }
-//         console.log(taskStorage);
-//     });
-// });
-//function to create the dropdown elements
+// function that creates dropdowns from the options provided
 function createDropdown(options, id) {
     const dropdown = document.createElement("select");
     dropdown.id = id;
@@ -104,152 +76,143 @@ function createDropdown(options, id) {
     return dropdown;
 }
 
-// Needs to create a task based on the option selected or the custom option
-function createTask(task, listElement) {
-    //create the task and delete button elements
-    const addTask = document.createElement("li");
-    const deleteTask = document.createElement("button");
+// function that creates a task given a task name status and priority
+function createTask(task, status, priority) {
+    const newTask = document.createElement("li");
+    newTask.textContent = task;
+    newTask.style.color = "#333";
+    const deleteBtn = document.createElement("button");
 
-    //create priority dropdown element
+    deleteBtn.textContent = "❌";
+    deleteBtn.classList.add("delete");
+
     const priorityOptions = [
         { value: "high", text: "High" },
         { value: "medium", text: "Medium" },
         { value: "low", text: "Low" }
-    ]
-    const priority = createDropdown(priorityOptions, "priority-list");
+    ];
 
-    //create status dropdown elements
     const statusOptions = [
         { value: "todo", text: "To Do" },
         { value: "doing", text: "Doing" },
         { value: "done", text: "Done" }
-    ]
-    const status = createDropdown(statusOptions, "status");
+    ];
 
-    //configure the delete button
-    deleteTask.textContent = "❌";
-    deleteTask.classList.add("delete");
-    // addDeleteFunctionality(deleteTask);
+    const statusEl = createDropdown(statusOptions, "task-status");
+    const priorityEl = createDropdown(priorityOptions, "task-priority");
 
-    //configure the task element
-    addTask.textContent = task;
-    addTask.classList.add("task-item");
-    addTask.appendChild(status);
-    addTask.appendChild(priority);
-    addTask.appendChild(deleteTask);
-    listElement.appendChild(addTask);
+    // Set the selected value for status and priority
+    statusEl.value = status;
+    priorityEl.value = priority;
 
-    addTask.priority = priority;
-    addTask.status = status;
+    newTask.appendChild(statusEl);
+    newTask.appendChild(priorityEl);
+    newTask.appendChild(deleteBtn);
+    newTask.setAttribute("data-id", task);
 
-    //push the addtask properties to taskObject
-    taskObject = {
-        task: task,
-        priority: priority.value,
-        status: status.value
-    };
-    taskStorage.push(taskObject);
-    console.log(taskStorage);
-
-    //save task to local storage
-    saveTasksToLocalStorage();
-
-    // handle priority and status
-    handlePriority(addTask);
-    handleStatus(addTask);
-
-
-    // Add event listener to the delete button
-    deleteTask.addEventListener("click", function () {
-        const taskItem = deleteTask.parentNode;
-        taskItem.parentNode.removeChild(taskItem);
-        // remove the task from the storage array
-        taskStorage.splice(taskStorage.indexOf(taskObject.task), 1);
-        saveTasksToLocalStorage();
-        console.log(taskStorage);
-    });
-}
-
-
-// no longer necessary
-// function to delete the task
-// function addDeleteFunctionality(deleteButton) {
-//     deleteButton.addEventListener("click", function () {
-//         const taskItem = deleteButton.parentNode;
-//         taskItem.parentNode.removeChild(taskItem);
-//         const taskIndex = taskStorage.indexOf(taskItem.textContent.replace("Delete", "").trim());
-//         if (taskIndex > -1) {
-//             taskStorage.splice(taskIndex, 1);
-//         }
-//         console.log(taskStorage);
-//     });
-// }
-
-// needs to have logic to store the user's tasks to local storage
-function saveTasksToLocalStorage() {
-    localStorage.setItem('tasks', JSON.stringify(taskStorage));
-}
-
-function loadTasksFromLocalStorage() {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
-    if (!storedTasks) {
-        return;
+    if (priority === "high") {
+        newTask.style.backgroundColor = "red";
+    } else if (priority === "medium") {
+        newTask.style.backgroundColor = "yellow";
+    } else if (priority === "low") {
+        newTask.style.backgroundColor = "lime";
     }
 
-    storedTasks.forEach(function (task) {
-        let listElement;
-        if (task.status === "todo") {
-            listElement = todo;
-        } else if (task.status === "doing") {
-            listElement = doing;
-        } else if (task.status === "done") {
-            listElement = done;
-        }
-        createTask(task.task, listElement);
+    if (status === "todo") {
+        todo.appendChild(newTask);
+    } else if (status === "doing") {
+        doing.appendChild(newTask);
+    } else if (status === "done") {
+        done.appendChild(newTask);
+    }
 
+    taskItems.push(newTask);
+}
+
+// function that renders the tasks from the objects in the taskStorage array
+function renderTasks() {
+    todo.innerHTML = "";
+    doing.innerHTML = "";
+    done.innerHTML = "";
+
+    taskStorage.forEach(taskObject => {
+        createTask(taskObject.task, taskObject.status, taskObject.priority);
     });
 }
 
-// needs to have logic to set the priority of the task
-function handlePriority(taskItem) {
-    taskItem.priority.addEventListener("change", function () {
-        if (taskItem.priority.value === "high") {
-            taskItem.style.backgroundColor = "red";
-        } else if (taskItem.priority.value === "medium") {
-            taskItem.style.backgroundColor = "yellow";
-        } else if (taskItem.priority.value === "low") {
-            taskItem.style.backgroundColor = "green";
-        }
-        // update the taskObject with the new priority
-        taskObject = {
-            task: taskItem.text,
-            priority: taskItem.priority.value,
-            status: taskItem.status.value
-        };
-        taskStorage.push(taskObject);
-        // update the taskStorage with the new taskObject
-        saveTasksToLocalStorage();
-    });
+// function that retrieves data from local storage and renders the tasks to the page
+function init() {
+    const storedTasks = getTasksFromLocalStorage();
+    if (storedTasks) {
+        taskStorage = storedTasks;
+    }
+    renderTasks();
 }
 
-// when the priortiy is changed the task moves to the appropriate list
-function handleStatus(taskItem) {
-    taskItem.status.addEventListener("change", function () {
-        if (taskItem.status.value === "todo") {
-            todo.appendChild(taskItem);
-        } else if (taskItem.status.value === "doing") {
-            doing.appendChild(taskItem);
-        } else if (taskItem.status.value === "done") {
-            done.appendChild(taskItem);
+// event listener that deletes a task when the delete button is clicked
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete")) {
+        const taskId = event.target.parentElement.getAttribute("data-id");
+        const taskIndex = taskStorage.findIndex(task => task.task === taskId);
+        taskStorage.splice(taskIndex, 1);
+        storeTasksInLocalStorage();
+        renderTasks();
+    }
+});
+
+// event listener that changes the status and priority of a task in the taskStorage array and on the page
+document.addEventListener("change", function (event) {
+    if (event.target.id === "task-status" || event.target.id === "task-priority") {
+        const taskId = event.target.parentElement.getAttribute("data-id");
+        const taskIndex = taskStorage.findIndex(task => task.task === taskId);
+        if (taskIndex !== -1) {
+            if (event.target.id === "task-status") {
+                taskStorage[taskIndex].status = event.target.value;
+            } else if (event.target.id === "task-priority") {
+                taskStorage[taskIndex].priority = event.target.value;
+            }
+            storeTasksInLocalStorage();
+            renderTasks();
         }
-        // update the taskObject with the new status
-        taskObject = {
-            task: taskItem.text,
-            priority: taskItem.priority.value,
-            status: taskItem.status.value
-        };
-        taskStorage.push(taskObject);
-        // update the taskStorage with the new taskObject
-        saveTasksToLocalStorage();
+    }
+});
+
+//event listener that opens the modal when the add button is clicked or the dropdown selection is made
+addBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    showModal();
+});
+
+//event listener that shows modal wehn add option is selected
+document.addEventListener("change", function (event) {
+    event.preventDefault();
+    if (event.target.value === "add") {
+        showModal();
+    }
+});
+
+//event listener that closes the modal when the close button is clicked
+closeModal.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+//event listener that renders a task when the submit button is clicked
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+    storeModalInput();
+    renderTasks();
+    modal.style.display = "none";
+});
+
+//event listener that renders a task when the dropdown selection is made
+document.querySelectorAll("#work-list, #personal-list, #family-list, #home-list, #health-list, #financial-list, #learning-list").forEach(function (selectElement) {
+    selectElement.addEventListener("change", function (event) {
+        event.preventDefault();
+        storeDropdownSelection(event.target);
+        renderTasks();
     });
-}
+
+});
+
+//call init functin to render tasks on page load
+init();
